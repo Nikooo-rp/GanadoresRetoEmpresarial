c81998b366ba834aff43ed795064f10f922d07b2
Nikooo-rp <nicolas.rojasp@upb.edu.co>
2026-04-13 19:11:41 -0500

    Add project files.

---
+++ GanadoresRetoEmpresarial/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GanadoresRetoEmpresarial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("meow");
+        }
+    }
+}
