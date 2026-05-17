0ff433ae34409e723733d719caa0913c2709f292
AGPUERTAS <jeronimo.rojasr.col@upb.edu.co>
2026-05-17 16:41:21 -0500

    .

--- GanadoresRetoEmpresarial/obj/Debug/net10.0/GanadoresRetoEmpresarial.AssemblyInfo.cs
+++ GanadoresRetoEmpresarial/obj/Debug/net10.0/GanadoresRetoEmpresarial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GanadoresRetoEmpresarial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d761f1e9d6447b3b8dfcef1a891caa26775d78c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c8ef90bbf3612792fbafd74a009ca4bbbcc1303")]
 [assembly: System.Reflection.AssemblyProductAttribute("GanadoresRetoEmpresarial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GanadoresRetoEmpresarial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
