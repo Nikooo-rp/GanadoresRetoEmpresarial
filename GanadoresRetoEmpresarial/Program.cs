61184fb07ec67eb831a6763f30c33edd1027da2c
Niko <nicolas.rojasp@upb.edu.co>
2026-05-18 21:36:09 -0500

    Comentarios en program

--- GanadoresRetoEmpresarial/Program.cs
+++ GanadoresRetoEmpresarial/Program.cs
@@ -4,10 +4,12 @@
     {
         static void Main(string[] args)
         {
+            // Instanciamos datos y menús.
             var data = new HotelData();
             MenuCliente menuCliente = new MenuCliente();
             MenuAdmin menuAdmin = new MenuAdmin();
 
+            // Base de datos inicial de usuarios y habitaciones.
             data.admins.AddRange(new List<Admin>
             {
                 new Admin("admin1", "adpass1"),
@@ -27,18 +29,23 @@
                 new Habitacion(201, 150, TipoHabitacion.Doble),
                 new Habitacion(202, 150, TipoHabitacion.Doble)
             });
+            // Los clientes los registran los recepcionistas.
+
             bool running = true;
             while (running)
             {
                 Console.Clear();
                 Console.WriteLine("=== Sistema de administración de Velisse Hotel ===");
                 string nombreUsuario = Admin.AskString("Ingresa tu nombre de usuario o 0 para salir:");
-                
+
+
+                // Si el usuario ingresa "0", se cierra el programa. running se vuelve false y se continúa al siguiente ciclo, que termina inmediatamente.
                 if (nombreUsuario == "0")
                 {
                     running = false;
                     continue;
                 }
+                // Creamos un usuario que empieza en null, luego se recorren las listas de usuarios en busca de un nombre que coincida con el ingresado.
                 Usuario? usuario = null;
                 if (data.clientes.Find(m => m.nombre == nombreUsuario) is Cliente cliente)
                 {
@@ -53,12 +60,14 @@
                     usuario = admin;
                 }
 
+                // Si se encontró un usuario, se habrá asignado, sino, seguirá siendo null y se repetirá el ciclo.
                 if (usuario == null)
                 {
                     Console.WriteLine("Usuario no encontrado, presiona cualquier tecla...");
                     Console.ReadKey();
                     continue;
                 }
+                // Si el usuario se encontró, se pide su contraseña. Si es válida, se muestra el menú correspondiente pasando el usuario actual y los datos.
                 else
                 {
                     string contraseña = Admin.AskString("Ingresa tu contraseña:");
