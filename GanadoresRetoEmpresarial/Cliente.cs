0ecdc78d3814e87ab23950ca79d114b44e858118
Niko <nicolas.rojasp@upb.edu.co>
2026-05-20 16:17:39 -0500

    implementado metodo crearReserva en Cliente

--- GanadoresRetoEmpresarial/Cliente.cs
+++ GanadoresRetoEmpresarial/Cliente.cs
@@ -38,7 +38,7 @@ namespace GanadoresRetoEmpresarial
             Console.WriteLine("Reserva cancelada exitosamente.");
         }
 
-        internal Reserva CrearReserva(DateTime fechaEntrada, DateTime fechaSalida, Habitacion habitacionSeleccionada)
+        public Reserva CrearReserva(DateTime fechaEntrada, DateTime fechaSalida, Habitacion habitacionSeleccionada)
         {
             return new Reserva(fechaEntrada, fechaSalida, habitacionSeleccionada.precioNoche);
         }
